b8dbd145654bb53f4cfcdcd55f6cf6c469816e86
stehessel <stephan@stehessel.de>
2020-02-04 11:51:23 +0100

    fix output in update.fish

--- program/shell/fish/config/functions/update.fish
+++ program/shell/fish/config/functions/update.fish
@@ -1,18 +1,18 @@
 function update -d "update all"
 	echo 'starting updates ...'
-	echo 'updating pacman and aur'
 	if command --search trizen >/dev/null do
+		echo 'updating pacman and aur ...'
 		trizen -Syu
 	end
 	if command --search brew >/dev/null do
-		echo 'updating homebrew'
+		echo 'updating homebrew ...'
 		brew update
 		brew upgrade
 		brew cleanup
-		echo 'updating Apple packages'
+		echo 'updating Apple packages ...'
 		/usr/sbin/softwareupdate -ia
 	end
-	echo 'updating fish shell'
+	echo 'updating fish shell ...'
 	fisher
 	fish_update_completions
 end
