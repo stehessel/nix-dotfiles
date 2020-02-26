89b6324c65ca48751eb221b7fba4560f8fb8f8a0
stehessel <stephan@stehessel.de>
2020-02-26 21:45:34 +0100

    move packages to common role

--- role/common/default.nix
+++ role/common/default.nix
@@ -24,6 +24,69 @@
 
 	nixpkgs.config.allowUnfree = true;
 
+	home.packages = with pkgs; [
+		# --- dev ---
+		ccls
+		editorconfig-core-c
+		gdb
+		jq
+		kakoune
+		rustup
+		tig
+		tokei
+		universal-ctags
+		xsv
+		# --- file-manager ---
+		lf
+		vifm
+		# --- file-viewer ---
+		glow
+		pandoc
+		# --- font ---
+		fira-code
+		font-awesome
+		hack-font
+		# --- nix ---
+		arion
+		lorri
+		niv
+		nox
+		pypi2nix
+		# --- shell ---
+		dash
+		fish
+		starship
+		# --- terminal ---
+		alacritty
+		kitty
+		# --- terminal tools ---
+		bat
+		broot
+		exa
+		fd
+		fzf
+		highlight
+		neofetch
+		ripgrep
+		skim
+		tmux
+		tre-command
+		# --- utility ---
+		bandwhich
+		direnv
+		du-dust
+		file
+		htop
+		hyperfine
+		jrnl
+		just
+		# procs
+		pueue
+		speedtest-cli
+		svgcleaner
+		taskwarrior
+		topgrade
+	];
 
 	programs.alacritty = {
 		enable = true;
