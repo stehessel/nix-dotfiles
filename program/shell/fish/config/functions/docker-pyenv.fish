18668e549877f3ecfce32954a203099fe605443c
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@jda.com>
2020-03-16 15:15:55 +0100

    changes to docker-pyenv

--- program/shell/fish/config/functions/docker-pyenv.fish
+++ program/shell/fish/config/functions/docker-pyenv.fish
@@ -3,5 +3,21 @@ function docker-pyenv -a container_id --description "Create python environment f
 		# Use latest container
 		set container_id (docker ps -lq)
 	end
-	docker exec -it $container_id /home/docker/venv3.6/bin/pip freeze > /tmp/requirements.txt
+
+	docker exec -it $container_id /home/docker/venv3.6/bin/pip freeze \
+		> /tmp/requirements.txt
+
+	conda deactivate
+	conda env remove -n docker
+	conda create -n docker python=3.6 -y
+	conda activate docker
+
+	set index_url https://software.z.westeurope.blue-yonder.cloud/platform_dev/Debian_9/+simple
+	cat /tmp/requirements.txt \
+		| xargs -n 1 -L 1 \
+		pip --cert $HOME/certs/Certificates.pem \
+			--trusted-host pypi.org \
+			--trusted-host files.pythonhosted.org \
+			install \
+				--extra-index-url $index_url
 end
