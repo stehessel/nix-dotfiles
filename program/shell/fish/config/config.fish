7a820a4cdbfdd87d69ac6ba9e8b6a026a5e025c5
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@jda.com>
2020-04-04 16:17:52 +0200

    add git checkout abbr

--- program/shell/fish/config/config.fish
+++ program/shell/fish/config/config.fish
@@ -59,11 +59,12 @@ abbr -a -U cfmc "$EDITOR ~/.config/ncmpcpp/config"
 if test "$TERMINAL" = "kitty"
 	abbr -a -U diff "kitty +kitten diff"
 end
-abbr -a -U gb "git branch"
-abbr -a -U gc "git commit -am"
-abbr -a -U gd "git diff"
-abbr -a -U gs "git status"
-abbr -a -U tl "tasklite"
+abbr -a -U gb  "git branch"
+abbr -a -U gc  "git commit -am"
+abbr -a -U gco "git checkout"
+abbr -a -U gd  "git diff"
+abbr -a -U gs  "git status"
+abbr -a -U tl  "tasklite"
 abbr -a -U corona "curl 'https://corona-stats.online?source=2&top=15'"
 
 # keybinds
