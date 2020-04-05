879f8bd9fe0991d04d57a2992eddb53b4276d286
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@jda.com>
2020-04-05 20:47:42 +0200

    clean up iron config

--- program/editor/neovim/config/plugins.lua
+++ program/editor/neovim/config/plugins.lua
@@ -1,13 +1,5 @@
 local iron = require('iron')
 
-iron.core.add_repl_definitions {
-	python = {
-		default = {
-			command = {"ipython"}
-		}
-	}
-}
-
 iron.core.set_config {
 	preferred = {
 		python = "ipython"
