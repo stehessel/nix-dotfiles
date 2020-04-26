e4a02b106f1b359e162c723826999a56cf831277
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2020-04-26 15:59:09 +0200

    fix neovim overlay

--- overlays.nix
+++ overlays.nix
@@ -1,7 +1,7 @@
 [
   (self: super:
   {
-    neovim = super.neovim.overrideAttrs (old: rec {
+    neovim = super.neovim-unwrapped.overrideAttrs (old: rec {
       version = "nightly";
       src = builtins.fetchGit {
         url = "https://github.com/neovim/neovim.git";
@@ -19,15 +19,4 @@
       };
     });
   })
-  # (self: super:
-  # {
-  #   fish = super.fish.overrideAttrs (old: rec {
-  #     pname = "fish";
-  #     version = "3.0.0";
-  #     src = super.fetchurl {
-  #       url = "https://github.com/fish-shell/fish-shell/releases/download/3.0.0/fish-3.0.0.tar.gz";
-  #       sha256 = "1kzjd0n0sfslkd36lzrvvvgy3qwkd9y466bkrqlnhd5h9dhx77ga";
-  #     };
-  #   });
-  # })
 ]
