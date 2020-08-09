9cfb62c76cefa80241875206e0ad69f56760043d
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2020-08-09 13:19:35 +0200

    add lua files

---
+++ program/editor/neovim/config/lua/lsp.lua
@@ -0,0 +1,9 @@
+require "nvim_lsp".bashls.setup {}
+require "nvim_lsp".ccls.setup {}
+require "nvim_lsp".dockerls.setup {}
+require "nvim_lsp".jsonls.setup {}
+require "nvim_lsp".pyls.setup {}
+-- require'nvim_lsp'.rls.setup{}
+require "nvim_lsp".rust_analyzer.setup {}
+-- require'nvim_lsp'.sumneko_lua.setup{}
+require "nvim_lsp".vimls.setup {}
