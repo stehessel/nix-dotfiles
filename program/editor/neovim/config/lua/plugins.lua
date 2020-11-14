e495a03f5bdba593698d2c64098213f66c934c04
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2020-11-14 12:42:11 +0100

    update telescope config

--- program/editor/neovim/config/lua/plugins.lua
+++ program/editor/neovim/config/lua/plugins.lua
@@ -119,9 +119,42 @@ return require("packer").startup(
             use {
                 "nvim-lua/telescope.nvim",
                 config = function()
+                    local actions = require("telescope.actions")
+                    local sorters = require("telescope.sorters")
+                    local themes = require("telescope.themes")
                     require("telescope").setup {
                         defaults = {
-                            shorten_path = false
+                            prompt_prefix = " >",
+                            winblend = 0,
+                            preview_cutoff = 120,
+                            scroll_strategy = "cycle",
+                            layout_strategy = "horizontal",
+                            layout_defaults = {
+                                horizontal = {
+                                    width_padding = 0.1,
+                                    height_padding = 0.1,
+                                    preview_width = 0.6
+                                },
+                                vertical = {
+                                    width_padding = 0.05,
+                                    height_padding = 1,
+                                    preview_height = 0.5
+                                }
+                            },
+                            sorting_strategy = "descending",
+                            prompt_position = "bottom",
+                            color_devicons = true,
+                            mappings = {
+                                i = {
+                                    ["<c-x>"] = false,
+                                    ["<c-s>"] = actions.goto_file_selection_split
+                                }
+                            },
+                            borderchars = {
+                                {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
+                                preview = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"}
+                            },
+                            file_sorter = sorters.get_fzy_sorter
                         }
                     }
 
