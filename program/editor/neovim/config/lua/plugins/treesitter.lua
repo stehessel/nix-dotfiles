2f1d0bc9e8df81da0658aad8f0063125cccd25eb
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2021-01-24 13:15:13 +0100

    update treesitter

--- program/editor/neovim/config/lua/plugins/treesitter.lua
+++ program/editor/neovim/config/lua/plugins/treesitter.lua
@@ -1,55 +1,56 @@
 require"nvim-treesitter.configs".setup {
-    highlight = {enable = true},
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "gl",
-            node_incremental = "i",
-            node_decremental = "o",
-            scope_incremental = "u",
-            scope_decremental = "d"
-        }
+  highlight = {enable = true},
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gl",
+      node_incremental = "i",
+      node_decremental = "o",
+      scope_incremental = "u",
+      scope_decremental = "d",
     },
-    refactor = {
-        highlight_definitions = {enable = false},
-        highlight_current_scope = {enable = false},
-        smart_rename = {enable = true, keymaps = {smart_rename = "<leader>R"}},
-        navigation = {
-            enable = true,
-            keymaps = {goto_definition = "gnd", list_definitions = "gnD"}
-        }
+  },
+  indent = {entable = true},
+  refactor = {
+    highlight_definitions = {enable = false},
+    highlight_current_scope = {enable = false},
+    smart_rename = {enable = true, keymaps = {smart_rename = "<leader>R"}},
+    navigation = {
+      enable = true,
+      keymaps = {goto_definition = "gnd", list_definitions = "gnD"},
     },
-    textobjects = {
-        enable = false,
-        disable = {},
-        keymaps = {
-            ["iL"] = {
-                -- you can define your own textobjects directly here
-                python = "(function_definition) @function"
-            },
-            -- or you use the queries from supported languages with textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["aC"] = "@class.outer",
-            ["iC"] = "@class.inner",
-            ["ac"] = "@conditional.outer",
-            ["ic"] = "@conditional.inner",
-            ["ae"] = "@block.outer",
-            ["ie"] = "@block.inner",
-            ["al"] = "@loop.outer",
-            ["il"] = "@loop.inner",
-            ["is"] = "@statement.inner",
-            ["as"] = "@statement.outer",
-            ["ad"] = "@comment.outer",
-            ["am"] = "@call.outer",
-            ["im"] = "@call.inner"
-        }
+  },
+  textobjects = {
+    enable = true,
+    disable = {},
+    keymaps = {
+      ["iL"] = {
+        -- you can define your own textobjects directly here
+        python = "(function_definition) @function",
+      },
+      -- or you use the queries from supported languages with textobjects.scm
+      ["af"] = "@function.outer",
+      ["if"] = "@function.inner",
+      ["aC"] = "@class.outer",
+      ["iC"] = "@class.inner",
+      ["ac"] = "@conditional.outer",
+      ["ic"] = "@conditional.inner",
+      ["ae"] = "@block.outer",
+      ["ie"] = "@block.inner",
+      ["al"] = "@loop.outer",
+      ["il"] = "@loop.inner",
+      ["is"] = "@statement.inner",
+      ["as"] = "@statement.outer",
+      ["ad"] = "@comment.outer",
+      ["am"] = "@call.outer",
+      ["im"] = "@call.inner",
     },
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false -- Whether the query persists across vim sessions
-    },
-    ensure_installed = "all"
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+  },
+  ensure_installed = "all",
 }
