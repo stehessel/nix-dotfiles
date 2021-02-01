449afe8236f4af45da164c9d44e2c799342b375f
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2021-02-01 21:47:02 +0100

    add git alias

--- dev/version-control/git/default-options.nix
+++ dev/version-control/git/default-options.nix
@@ -24,12 +24,22 @@
   ];
 
   extraConfig = {
+    alias = {
+      # Usage: git assume fileName
+      assume = "update-index --assume-unchanged";
+      # Usage: git noassume fileName
+      noassume = "update-index --no-assume-unchanged";
+      # Another alias because I cannot remember un/no either
+      unassume = "!git noassume";
+      # List all currently git assumed files
+      assumed = "!git ls-files -v | grep ^h | cut -c 3-";
+    };
     color = {
       ui = "true";
       diff-highlight = {
-        oldNormal    = "red bold";
+        oldNormal = "red bold";
         oldHighlight = "red bold 52";
-        newNormal    = "green bold";
+        newNormal = "green bold";
         newHighlight = "green bold 22";
       };
       diff = {
