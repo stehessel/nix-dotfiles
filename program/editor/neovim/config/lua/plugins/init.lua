6254a49d3eaaed551ad62dbff1662e7de11e0eeb
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2021-04-02 16:11:57 +0200

    add binds

--- program/editor/neovim/config/lua/plugins/init.lua
+++ program/editor/neovim/config/lua/plugins/init.lua
@@ -583,6 +583,50 @@ return require("packer").startup({
       config = function()
         vimp.nnoremap({"silent"}, "<leader>S", require("spectre").open)
         vimp.vnoremap({"silent"}, "<leader>S", require("spectre").open_visual)
+        require("spectre").setup({
+          mapping = {
+            ["delete_line"] = {
+              map = "dd",
+              cmd = "<cmd>lua require('spectre').delete()<CR>",
+              desc = "delete current item",
+            },
+            ["enter_file"] = {
+              map = "<cr>",
+              cmd = "<cmd>lua require('spectre.actions').select_entry()<CR>",
+              desc = "goto current file",
+            },
+            ["send_to_qf"] = {
+              map = "rq",
+              cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+              desc = "send all item to quickfix",
+            },
+            ["replace_cmd"] = {
+              map = "rc",
+              cmd = "<cmd>lua require('spectre.actions').replace_cmd()<CR>",
+              desc = "input replace vim command",
+            },
+            ["show_option_menu"] = {
+              map = "to",
+              cmd = "<cmd>lua require('spectre').show_options()<CR>",
+              desc = "show option",
+            },
+            ["run_replace"] = {
+              map = "rS",
+              cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
+              desc = "replace all",
+            },
+            ["toggle_ignore_case"] = {
+              map = "ti",
+              cmd = "<cmd>lua require('spectre').change_options('ignore-case')<CR>",
+              desc = "toggle ignore case",
+            },
+            ["toggle_ignore_hidden"] = {
+              map = "th",
+              cmd = "<cmd>lua require('spectre').change_options('hidden')<CR>",
+              desc = "toggle search hidden",
+            },
+          },
+        })
       end,
     }
     use {"inkarkat/vim-SearchAlternatives", requires = "inkarkat/vim-ingo-library"}
