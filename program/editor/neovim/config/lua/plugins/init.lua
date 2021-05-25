41b14ddc6bbc393147a64eb1c3494babd75b71ed
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2021-05-25 18:18:59 +0200

    update lsp diagnostics

--- program/editor/neovim/config/lua/plugins/init.lua
+++ program/editor/neovim/config/lua/plugins/init.lua
@@ -209,13 +209,7 @@ return require("packer").startup({
       "glepnir/lspsaga.nvim",
       cmd = "Lspsaga",
       config = function()
-        require("lspsaga").init_lsp_saga({
-          use_saga_diagnostic_sign = true,
-          error_sign = "",
-          warn_sign = "",
-          hint_sign = "",
-          infor_sign = "",
-        })
+        require("lspsaga").init_lsp_saga({ use_saga_diagnostic_sign = false })
       end,
     })
     use({
