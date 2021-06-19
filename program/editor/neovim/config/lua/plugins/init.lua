029160bddd066914968a8559f8e780f3aa5a0b95
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2021-06-19 14:22:15 +0200

    top input

--- program/editor/neovim/config/lua/plugins/init.lua
+++ program/editor/neovim/config/lua/plugins/init.lua
@@ -112,7 +112,7 @@ return require("packer").startup({
           defaults = {
             color_devicons = true,
             scroll_strategy = "cycle",
-            -- prompt_position = "top",
+            prompt_position = "top",
             -- sorting_strategy = "ascending",  -- breaks Telescope
             prompt_prefix = "❯ ",
             selection_caret = "❯ ",
