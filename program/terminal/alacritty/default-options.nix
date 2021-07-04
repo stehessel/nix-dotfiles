0bf0a8de61eb746b4a04a3336dd760d898921783
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2021-07-04 20:31:49 +0200

    alacritty color settings

--- program/terminal/alacritty/default-options.nix
+++ program/terminal/alacritty/default-options.nix
@@ -15,13 +15,33 @@
     bold_italic.style = "Medium";
   };
 
-  mouse = {
-    url = {
-      launcher = {
-        program = "open";
-        args = [ ];
+
+  colors = {
+    primary =
+      {
+        background = "#000000";
+        foreground = "#BABABA";
       };
-      modifiers = "None";
+
+    normal = {
+      black = "#000000";
+      red = "#E8341C";
+      green = "#68C256";
+      yellow = "#F2D42C";
+      blue = "#1C98E8";
+      magenta = "#8E69C9";
+      cyan = "#1C98E8";
+      white = "#BABABA";
+    };
+    bright = {
+      black = "#666666";
+      red = "#E05A4F";
+      green = "#77B869";
+      yellow = "#EFD64B";
+      blue = "#387CD3";
+      magenta = "#957BBE";
+      cyan = "#3D97E2";
+      white = "#BABABA";
     };
   };
 
