576ce3e12c840da206d8924ebaa7c04916fa350b
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2021-09-10 00:13:29 +0200

    vim

--- program/editor/neovim/config/lua/plugins/init.lua
+++ program/editor/neovim/config/lua/plugins/init.lua
@@ -338,8 +338,8 @@ return require("packer").startup({
         local cmp = require("cmp")
         local luasnip = require("luasnip")
         local check_back_space = function()
-          local col = vim.fn.col(".") - 1
-          return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col == 0 or vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") ~= nil
         end
 
         cmp.setup({
@@ -370,22 +370,25 @@ return require("packer").startup({
             }),
             ["<Tab>"] = function(_, fallback)
               if vim.fn.pumvisible() == 1 then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
               elseif check_back_space() then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, true, true), "n")
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, true, true), "n")
               elseif luasnip.expand_or_jumpable() then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                vim.api.nvim_feedkeys(
+                  vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+                  ""
+                )
               else
                 fallback()
               end
             end,
             ["<S-Tab>"] = function(_, fallback)
               if vim.fn.pumvisible() == 1 then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
               elseif check_back_space() then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-h>", true, true, true), "n")
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-h>", true, true, true), "n")
               elseif luasnip.jumpable(-1) then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
               else
                 fallback()
               end
