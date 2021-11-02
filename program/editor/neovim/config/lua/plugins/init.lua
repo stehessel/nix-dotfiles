176e45c8f3033378b7c9a839ea089e9cba0e63d4
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2021-11-02 23:10:59 +0100

    cmp

--- program/editor/neovim/config/lua/plugins/init.lua
+++ program/editor/neovim/config/lua/plugins/init.lua
@@ -382,8 +382,24 @@ return require("packer").startup({
               behavior = cmp.ConfirmBehavior.Replace,
               select = true,
             }),
-            ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-            ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+            ["<Tab>"] = function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              else
+                fallback()
+              end
+            end,
+            ["<S-Tab>"] = function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end,
           },
           snippet = {
             expand = function(args)
