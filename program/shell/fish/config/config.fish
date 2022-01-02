b3ea51559884c16703f63e4cf33df290392147c1
Stephan Heßelmann <stehessel@redhat.com>
2022-01-02 19:54:58 +0100

    add jdk path

--- program/shell/fish/config/config.fish
+++ program/shell/fish/config/config.fish
@@ -40,6 +40,7 @@ switch (uname)
     case Darwin
         fish_add_path /usr/local/opt/coreutils/libexec/gnubin
         fish_add_path /usr/local/opt/gnu-sed/libexec/gnubin
+        fish_add_path /usr/local/opt/openjdk/bin
         set -x LC_ALL "en_US.UTF-8"
         set -x LANG "en_US.UTF-8"
         ln -s "/Applications/Firefox.app/Contents/MacOS/firefox" "$HOME/.local/bin/firefox" 2>/dev/null
