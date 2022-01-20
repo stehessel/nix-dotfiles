5c45ea03a8e95feaa7c1912dacbe2cb244738bc3
Stephan Heßelmann <shesselm@redhat.com>
2022-01-20 01:30:31 +0100

    update keyboard

--- systems/darwin/configuration.nix
+++ systems/darwin/configuration.nix
@@ -24,32 +24,39 @@
     imports = [ /Users/stephan/nix-home/macos.nix ];
   };
 
-  system.defaults = {
-    dock = {
-      autohide = true;
-      autohide-delay = "0.0";
-      autohide-time-modifier = "1.0";
-      tilesize = 75;
-      static-only = false;
-      showhidden = false;
-      show-recents = false;
-      show-process-indicators = true;
-      orientation = "bottom";
-      mru-spaces = false;
+  system = {
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
     };
 
-    NSGlobalDomain = {
-      "com.apple.sound.beep.feedback" = 0;
-      "com.apple.sound.beep.volume" = "0.000";
-      "com.apple.swipescrolldirection" = false;
-      # Allow key repeat
-      ApplePressAndHoldEnabled = false;
-      # Delay before repeating keystrokes
-      InitialKeyRepeat = 10;
-      # Delay between repeated keystrokes upon holding a key
-      KeyRepeat = 1;
-      AppleShowAllExtensions = true;
-      AppleShowScrollBars = "Automatic";
+    defaults = {
+      dock = {
+        autohide = true;
+        autohide-delay = "0.0";
+        autohide-time-modifier = "1.0";
+        tilesize = 75;
+        static-only = false;
+        showhidden = false;
+        show-recents = false;
+        show-process-indicators = true;
+        orientation = "bottom";
+        mru-spaces = false;
+      };
+
+      NSGlobalDomain = {
+        "com.apple.sound.beep.feedback" = 0;
+        "com.apple.sound.beep.volume" = "0.000";
+        "com.apple.swipescrolldirection" = false;
+        # Allow key repeat
+        ApplePressAndHoldEnabled = false;
+        # Delay before repeating keystrokes
+        InitialKeyRepeat = 10;
+        # Delay between repeated keystrokes upon holding a key
+        KeyRepeat = 1;
+        AppleShowAllExtensions = true;
+        AppleShowScrollBars = "Automatic";
+      };
     };
   };
 
