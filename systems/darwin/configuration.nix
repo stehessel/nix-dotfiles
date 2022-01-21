288e0eb05246ffaffc2ab808fbc5562f5ad3091b
Stephan Heßelmann <shesselm@redhat.com>
2022-01-21 21:31:07 +0100

    add touch id

--- systems/darwin/configuration.nix
+++ systems/darwin/configuration.nix
@@ -60,6 +60,8 @@
     };
   };
 
+  security.pam.enableSudoTouchIdAuth = true;
+
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
