d0e1eace288ef0425b8e26f26cd64ee7628ed1ee
Stephan Heßelmann <shesselm@redhat.com>
2022-01-22 00:55:37 +0100

    add boto home

--- program/shell/fish/config/config.fish
+++ program/shell/fish/config/config.fish
@@ -19,6 +19,7 @@ set -x fzf_fd_opts "--type=file"
 set -x fzf_preview_dir_cmd "exa --all --color=always --group-directories-first"
 # fzf_configure_bindings --directory=\cf
 
+set -x BOTO_CONFIG "$XDG_CONFIG_HOME/boto/config"
 set -x CARGO_HOME "$XDG_CONFIG_HOME/cargo"
 set -x GOPATH "$HOME/go"
 set -x MINIKUBE_HOME "$XDG_CONFIG_HOME"
