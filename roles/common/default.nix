daed330ef2bcf1e27c3c783277a9e4b1a5fdcf26
Stephan Heßelmann <shesselm@redhat.com>
2022-01-27 00:51:17 +0100

    starship git status

--- roles/common/default.nix
+++ roles/common/default.nix
@@ -95,6 +95,13 @@
 
     starship = {
       enable = true;
+      settings = {
+        git_status = {
+          ahead = "⇡\${count}";
+          diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
+          behind = "⇣\${count}";
+        };
+      };
     };
   };
 
