c71fec7ef63bc795128087867b448335e254c7fa
Stephan Heßelmann <shesselm@redhat.com>
2022-01-27 18:22:52 +0100

    neovim overlay

--- flake.nix
+++ flake.nix
@@ -11,55 +11,61 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    neovim-nightly-overlay = {
-      url = "github:nix-community/neovim-nightly-overlay";
+    neovim-flake = {
+      url = "github:neovim/neovim?dir=contrib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, darwin, home-manager, neovim-nightly-overlay }: {
-    darwinConfigurations."shesselm-mac" = darwin.lib.darwinSystem {
-      system = "x86_64-darwin";
-      modules = [
-        ./systems/darwin
+  outputs = { self, nixpkgs, darwin, home-manager, neovim-flake }:
+    let
+      neovim-overlay = final: prev: {
+        inherit (neovim-flake.packages.${ prev.system}) neovim;
+      };
+    in
+    {
+      darwinConfigurations."shesselm-mac" = darwin.lib.darwinSystem {
+        system = "x86_64-darwin";
+        modules = [
+          ./systems/darwin
 
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.stephan = {
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.stephan = {
+              imports = [
+                ./profiles/redhat
+                ./roles/work-macos
+              ];
+            };
+            nixpkgs = {
+              overlays = [ neovim-overlay ];
+            };
+            users.users.stephan = {
+              name = "stephan";
+              home = "/Users/stephan";
+            };
+          }
+        ];
+      };
+
+      homeConfigurations = {
+        stephan = home-manager.lib.homeManagerConfiguration {
+          configuration = {
             imports = [
-              ./profiles/redhat
-              ./roles/work-macos
+              ./profiles/stehessel
+              ./roles/linux
             ];
           };
           nixpkgs = {
-            overlays = []; # [ neovim-nightly-overlay.overlay ];
+            overlays = [ neovim-overlay ];
           };
-          users.users.stephan = {
-            name = "stephan";
-            home = "/Users/stephan";
-          };
-        }
-      ];
-    };
-
-    homeConfigurations = {
-      stephan = home-manager.lib.homeManagerConfiguration {
-        configuration = {
-          imports = [
-            ./profiles/stehessel
-            ./roles/linux
-          ];
-          nixpkgs = {
-            overlays = [ neovim-nightly-overlay.overlay ];
-          };
+          homeDirectory = "/home/stephan";
+          stateVersion = "22.05";
+          username = "stephan";
+          system = "x86_64-linux";
         };
-        homeDirectory = "/home/stephan";
-        stateVersion = "22.05";
-        username = "stephan";
-        system = "x86_64-linux";
       };
     };
-  };
 }
