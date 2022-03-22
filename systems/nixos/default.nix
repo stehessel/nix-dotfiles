d1d1f4ce481c2611ba651fda3a9f8795d6a830e5
stehessel <stephan@stehessel.de>
2022-03-22 15:58:10 +0100

    use systemd-network

--- systems/nixos/default.nix
+++ systems/nixos/default.nix
@@ -24,12 +24,27 @@
     };
     wireless.iwd.enable = true;
 
+    dhcpcd.enable = false;
+
     # The global useDHCP flag is deprecated, therefore explicitly set to false here.
     # Per-interface useDHCP will be mandatory in the future, so this generated config
     # replicates the default behaviour.
     useDHCP = false;
     interfaces.enp4s0.useDHCP = true;
     interfaces.wlp5s0.useDHCP = true;
+  };
+  systemd.network = {
+    enable = true;
+    networks = {
+      internet0 = {
+        matchConfig = {
+          Name = "enp4s0 wlp5s0";
+        };
+        networkConfig = {
+          DHCP = "ipv6";
+        };
+      };
+    };
   };
   programs.nm-applet.enable = true;
 
