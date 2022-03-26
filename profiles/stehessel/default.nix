7c9b71fee38cc86a07a75ba65801c0f58d55b335
stehessel <stephan@stehessel.de>
2022-03-26 21:28:15 +0100

    add ssh

--- profiles/stehessel/default.nix
+++ profiles/stehessel/default.nix
@@ -5,15 +5,29 @@
     ../../services/calcurse
   ];
 
+  programs = {
+    git = {
+      userEmail = "stephan@stehessel.de";
+      userName = "stehessel";
+    };
+    ssh = {
+      enable = true;
+      matchBlocks = {
+        "server" = {
+          addressFamily = "inet";
+          hostname = "hesselmann.cloud";
+          identitiesOnly = true;
+          identityFile = "~/.ssh/stehessel-server.pub";
+          port = 50022;
+          user = "stephan";
+        };
+      };
+    };
+  };
   services.mbsync = {
-    enable = true;
+    enable = false;
     configFile = config/mbsync/mbsyncrc;
     frequency = "*:0/1";
   };
   xdg.configFile."mbsync/mbsyncrc".source = config/mbsync/mbsyncrc;
-
-  programs.git = {
-    userEmail = "stephan@stehessel.de";
-    userName = "stehessel";
-  };
 }
