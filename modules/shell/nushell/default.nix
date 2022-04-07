8c72d6b6dbf1742c9a47e3577114b7258e03c587
Stephan Heßelmann <shesselm@redhat.com>
2022-04-07 14:12:14 +0200

    add nu config

--- modules/shell/nushell/default.nix
+++ modules/shell/nushell/default.nix
@@ -2,5 +2,13 @@
 {
   programs.nushell = {
     enable = true;
+    settings = {
+      edit_mode = "nvim";
+      prompt = "starship prompt";
+      startup = [
+        "alias la [] { ls -a }"
+        "alias e [msg] { echo $msg }"
+      ];
+    };
   };
 }
