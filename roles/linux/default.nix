2fd33a59d2fca8a5f11f074b4dde5e2447dd7914
stehessel <stephan@stehessel.de>
2022-04-09 16:03:50 +0200

    add back user systemd

--- roles/linux/default.nix
+++ roles/linux/default.nix
@@ -2,14 +2,14 @@
 {
   imports = [
     ../common
-    # ../../modules/desktop/dunst
+    ../../modules/desktop/dunst
     ../../modules/desktop/mime-apps
     ../../modules/desktop/rofi
-    # ../../modules/desktop/waybar
+    ../../modules/desktop/waybar
     ../../modules/desktop/window-manager/river
     ../../modules/file-viewer/sxiv
     ../../modules/file-viewer/zathura
-    # ../../modules/networking/syncthing
+    ../../modules/networking/syncthing
     ../../modules/terminal/foot
   ];
 
@@ -57,14 +57,14 @@
     home-manager.enable = true;
   };
 
-  # systemd.user.targets = {
-  #   tray = {
-  #     Unit = {
-  #       Description = "Home Manager System Tray";
-  #       Requires = [ "graphical-session-pre.target" ];
-  #     };
-  #   };
-  # };
+  systemd.user.targets = {
+    tray = {
+      Unit = {
+        Description = "Home Manager System Tray";
+        Requires = [ "graphical-session-pre.target" ];
+      };
+    };
+  };
 
   xdg.configFile."fish/fish_plugins".source = ../../modules/shell/fish/linux/fish_plugins;
 }
