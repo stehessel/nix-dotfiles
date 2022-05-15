9a7cc4f63dd081642f82c6fb8b89bbc74da02603
Stephan Heßelmann <shesselm@redhat.com>
2022-05-16 00:34:43 +0200

    add buf null-ls

--- modules/editor/neovim/config/lua/plugins/init.lua
+++ modules/editor/neovim/config/lua/plugins/init.lua
@@ -281,6 +281,7 @@ return require("packer").startup({
             builtins.code_actions.gitsigns,
             builtins.code_actions.refactoring,
             builtins.code_actions.statix,
+            builtins.diagnostics.buf,
             builtins.diagnostics.eslint.with({ command = "eslint_d" }),
             builtins.diagnostics.golangci_lint,
             builtins.diagnostics.hadolint,
