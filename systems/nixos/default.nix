9e1cb15fdc25411c4593a213ccba7fce66b67edf
Stephan Heßelmann <shesselm@redhat.com>
2022-06-25 00:41:50 +0200

    try content addressed derivations

--- systems/nixos/default.nix
+++ systems/nixos/default.nix
@@ -89,17 +89,15 @@
 
   nix = {
     binaryCaches = [
-      # See https://nixos.wiki/wiki/Maintainers:Fastly#Cache_v2_plans
-      "https://aseipp-nix-cache.freetls.fastly.net"
-      "https://nix-community.cachix.org"
+      "https://cache.ngi0.nixos.org"
     ];
     binaryCachePublicKeys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
     ];
     extraOptions = ''
       auto-optimise-store = true
-      experimental-features = nix-command flakes
+      contentAddressedByDefault = true
+      experimental-features = ca-derivations ca-references nix-command flakes
       keep-going = true
     '';
     gc = {
