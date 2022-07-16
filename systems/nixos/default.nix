bc5a2d8e2343bbe7003d637f77250b418c3d96a7
stehessel <stephan@stehessel.de>
2022-07-16 16:18:18 +0200

    remove duplicate definition

--- systems/nixos/default.nix
+++ systems/nixos/default.nix
@@ -71,26 +71,6 @@
     wrapperFeatures.gtk = true;
   };
 
-  environment.systemPackages = with pkgs; [
-    (river.overrideAttrs (prevAttrs: rec {
-      postInstall =
-        let
-          riverSession = ''
-            [Desktop Entry]
-            Name=River
-            Comment=Dynamic Wayland compositor
-            Exec=river
-            Type=Application
-          '';
-        in
-        ''
-          mkdir -p $out/share/wayland-sessions
-          echo "${riverSession}" > $out/share/wayland-sessions/river.desktop
-        '';
-      passthru.providedSessions = [ "river" ];
-    }))
-  ];
-
   services.xserver = {
     enable = true;
 
