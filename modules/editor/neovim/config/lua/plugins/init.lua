277cdc29ecd86be51607c0dd689fb256d7c0f2b7
Stephan Heßelmann <shesselm@redhat.com>
2022-07-22 14:16:25 +0200

    add context line

--- modules/editor/neovim/config/lua/plugins/init.lua
+++ modules/editor/neovim/config/lua/plugins/init.lua
@@ -851,6 +851,10 @@ return require("packer").startup({
         vim.g.indent_blankline_buftype_exclude = { "nofile", "prompt", "terminal" }
         vim.g.indent_blankline_char = "¦"
         vim.g.indent_blankline_filetype_exclude = { "packer", "startify" }
+        require("indent_blankline").setup({
+          show_current_context = true,
+          show_current_context_start = true,
+        })
       end,
     })
     use({ "Darazaki/indent-o-matic" })
