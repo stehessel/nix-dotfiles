d42f12db75f2d2592072987623c99d48d26df607
stehessel <stephan@stehessel.de>
2022-11-06 03:26:44 +0100

    add hcloud ssh key

--- systems/nixos/sops.nix
+++ systems/nixos/sops.nix
@@ -27,6 +27,18 @@
         owner = config.users.users.stephan.name;
         inherit (config.users.users.stephan.group);
       };
+      "ssh/hcloud_k8s/public" = {
+        path = "/etc/ssh/ssh_hcloud_k8s_key.pub";
+        sopsFile = ../../secrets/ssh.yaml;
+        owner = config.users.users.stephan.name;
+        inherit (config.users.users.stephan.group);
+      };
+      "ssh/hcloud_k8s/private" = {
+        path = "/etc/ssh/ssh_hcloud_k8s_key";
+        sopsFile = ../../secrets/ssh.yaml;
+        owner = config.users.users.stephan.name;
+        inherit (config.users.users.stephan.group);
+      };
       "ssh/server/public" = {
         path = "/etc/ssh/ssh_server_key.pub";
         sopsFile = ../../secrets/ssh.yaml;
