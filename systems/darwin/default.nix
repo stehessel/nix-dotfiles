110e0e79391eeb610ff83af6e26a3dc22ce5d348
Stephan Heßelmann <shesselm@redhat.com>
2022-12-13 22:06:42 +0100

    update nix

--- systems/darwin/default.nix
+++ systems/darwin/default.nix
@@ -168,10 +168,12 @@
         "https://aseipp-nix-cache.freetls.fastly.net"
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+        "https://stackrox.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "stackrox.cachix.org-1:Wnn8TKAitOTWKfTvvHiHzJjXy0YfiwoK6rrVzXt/trA="
       ];
       sandbox = true;
     };
