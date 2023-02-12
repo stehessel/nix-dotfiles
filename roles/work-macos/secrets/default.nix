8b9296adce09f051c3df7935c4d314bf6023f0a1
Stephan Heßelmann <shesselm@redhat.com>
2023-02-12 15:39:33 +0100

    add secrets

--- roles/work-macos/secrets/default.nix
+++ roles/work-macos/secrets/default.nix
@@ -9,6 +9,36 @@
       "bitwarden/api/secret" = {
         path = "${config.home.homeDirectory}/dev/secrets/bitwarden/secret";
       };
+      "aws/dev/accessKeyId" = {
+        path = "${config.home.homeDirectory}/dev/secrets/aws/dev/accessKeyId";
+      };
+      "aws/dev/accountId" = {
+        path = "${config.home.homeDirectory}/dev/secrets/aws/dev/accountId";
+      };
+      "aws/dev/secretAccessKey" = {
+        path = "${config.home.homeDirectory}/dev/secrets/aws/dev/secretAccessKey";
+      };
+      "docker/stehessel/user" = {
+        path = "${config.home.homeDirectory}/dev/secrets/docker/user";
+      };
+      "docker/stehessel/password" = {
+        path = "${config.home.homeDirectory}/dev/secrets/docker/password";
+      };
+      "openshift/pullSecret" = {
+        path = "${config.home.homeDirectory}/dev/secrets/openshift/pullSecret";
+      };
+      "quay.io/stehessel/user" = {
+        path = "${config.home.homeDirectory}/dev/secrets/quay.io/user";
+      };
+      "quay.io/stehessel/password" = {
+        path = "${config.home.homeDirectory}/dev/secrets/quay.io/password";
+      };
+      "infractl/token" = {
+        path = "${config.home.homeDirectory}/dev/secrets/infractl/token";
+      };
+      "jira/token" = {
+        path = "${config.home.homeDirectory}/dev/secrets/jira/token";
+      };
     };
   };
 }
