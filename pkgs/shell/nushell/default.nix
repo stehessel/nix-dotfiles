d91e14bb75f6f803e452a8df032ec044bbe0a0cd
Stephan Heßelmann <shesselm@redhat.com>
2023-02-13 14:52:58 +0100

    nu settings

--- pkgs/shell/nushell/default.nix
+++ pkgs/shell/nushell/default.nix
@@ -1,5 +1,13 @@
 _: {
   programs.nushell = {
     enable = true;
+    configFile = {
+      text = ''
+        let-env config = {
+          edit_mode: vi
+          show_banner: false
+        }
+      '';
+    };
   };
 }
