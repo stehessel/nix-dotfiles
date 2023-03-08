c4848c1925b2ef0a4a87e6dde61593e835774897
Stephan Heßelmann <shesselm@redhat.com>
2023-03-08 01:53:47 +0100

    update cmp settings

--- pkgs/editor/neovim/config/lua/plugins/completion.lua
+++ pkgs/editor/neovim/config/lua/plugins/completion.lua
@@ -30,7 +30,7 @@ return {
             },
           }),
         },
-        mapping = {
+        mapping = cmp.mapping.preset.insert({
           ["<C-p>"] = cmp.mapping.select_prev_item(),
           ["<C-n>"] = cmp.mapping.select_next_item(),
           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
@@ -41,7 +41,7 @@ return {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
           }),
-          ["<Tab>"] = function(fallback)
+          ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
@@ -51,8 +51,8 @@ return {
             else
               fallback()
             end
-          end,
-          ["<S-Tab>"] = function(fallback)
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
@@ -60,8 +60,8 @@ return {
             else
               fallback()
             end
-          end,
-        },
+          end, { "i", "s" }),
+        }),
         snippet = {
           expand = function(args)
             require("luasnip").lsp_expand(args.body)
