c26f6a98878efe2c916e88a2133f01fa0dfa085a
stehessel <stephan@stehessel.de>
2023-03-19 23:54:15 +0100

    disable keyfile

--- systems/thinkpad/default.nix
+++ systems/thinkpad/default.nix
@@ -11,13 +11,13 @@
       luks.devices."root" = {
         device = "/dev/disk/by-uuid/c8b25a10-6e4a-4832-af07-10e1d048dfee";
         preLVM = true;
-        keyFile = "/keyfile0.bin";
+        # keyFile = "/keyfile0.bin";
         allowDiscards = true;
       };
 
-      secrets = {
-        "keyfile0.bin" = "/etc/secrets/initrd/keyfile0.bin";
-      };
+      # secrets = {
+      #   "keyfile0.bin" = "/etc/secrets/initrd/keyfile0.bin";
+      # };
     };
   };
 
