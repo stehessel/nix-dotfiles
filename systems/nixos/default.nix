38158acc0642d4ac1e29a3174f87f2fea4aac9e4
stehessel <stephan@stehessel.de>
2023-03-20 16:43:54 +0100

    remove key file

--- systems/nixos/default.nix
+++ systems/nixos/default.nix
@@ -48,6 +48,15 @@
     sbctl
   ];
 
+  fileSystems."/efi" = {
+    options = [
+      "defaults"
+      "noauto"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=1"
+    ];
+  };
+
   hardware = {
     bluetooth.enable = true;
     enableRedistributableFirmware = true;
--- systems/thinkpad/default.nix
+++ systems/thinkpad/default.nix
@@ -11,23 +11,9 @@
       luks.devices."root" = {
         device = "/dev/disk/by-uuid/c8b25a10-6e4a-4832-af07-10e1d048dfee";
         preLVM = true;
-        # keyFile = "/keyfile0.bin";
         allowDiscards = true;
       };
-
-      # secrets = {
-      #   "keyfile0.bin" = "/etc/secrets/initrd/keyfile0.bin";
-      # };
     };
-  };
-
-  fileSystems."/efi" = {
-    options = [
-      "defaults"
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=1"
-    ];
   };
 
   networking.hostName = "thinkpad";
