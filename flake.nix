475707521b67cc8832c707ae1b96ca953dda21f3
stehessel <stephan@stehessel.de>
2023-03-23 03:36:47 +0100

    use hardened profile

--- flake.nix
+++ flake.nix
@@ -54,6 +54,7 @@
       };
       modules = [
         inputs.lanzaboote.nixosModules.lanzaboote
+        "${nixpkgs}/nixos/modules/profiles/hardened.nix"
         ./systems/thinkpad
         flakeRegistry
         inputs.hyprland.nixosModules.default
