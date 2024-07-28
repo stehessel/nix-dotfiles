396c458b61c8173edfc4eb9b242948d5820bf81c
Stephan Heßelmann <shesselm@redhat.com>
2024-07-28 13:40:40 +0200

    update vim

--- pkgs/editor/neovim/config/lua/plugins/completion.lua
+++ pkgs/editor/neovim/config/lua/plugins/completion.lua
@@ -99,7 +99,11 @@ return {
     end,
     dependencies = {
       { "FelipeLema/cmp-async-path" },
-      { "L3MON4D3/LuaSnip" },
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+      },
       { "hrsh7th/cmp-nvim-lsp" },
       { "hrsh7th/cmp-nvim-lua" },
       { "hrsh7th/cmp-path" },
