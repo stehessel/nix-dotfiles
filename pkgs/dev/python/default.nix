80141ad8c11d320396a4d573695cb682bb477ab5
Stephan Heßelmann <shesselm@redhat.com>
2024-12-09 23:30:58 +0100

    add ruff

--- pkgs/dev/python/default.nix
+++ pkgs/dev/python/default.nix
@@ -5,7 +5,7 @@
 }: {
   imports = [
     ./flake8
-    # ./isort
+    ./isort
     ./mypy
     ./pycodestyle
   ];
@@ -16,6 +16,7 @@
       black
       pipx
       pyright
+      ruff
       uv
     ];
     sessionVariables = {
