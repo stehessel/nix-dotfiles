b26eab967546d4f522764465cc8eb1a9b15114f6
Stephan Heßelmann <shesselm@redhat.com>
2025-02-28 16:34:35 +0100

    update blink config

--- pkgs/editor/neovim/config/lua/plugins/completion.lua
+++ pkgs/editor/neovim/config/lua/plugins/completion.lua
@@ -1,139 +1,42 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
-      local lspkind = require("lspkind")
-      local luasnip = require("luasnip")
-
-      local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      cmp.setup({
-        completion = {
-          keyword_length = 1,
-        },
-        formatting = {
-          format = lspkind.cmp_format({
-            with_text = true,
-            menu = {
-              fuzzy_buffer = "[Buf]",
-              git = "[Git]",
-              luasnip = "[Snip]",
-              nvim_lsp = "[LSP]",
-              nvim_lua = "[Lua]",
-              neorg = "[Neorg]",
-              orgmode = "[Org]",
-              path = "[Path]",
-            },
-          }),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.close(),
-          ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        sorting = {
-          comparators = {
-            require("cmp_fuzzy_buffer.compare"),
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            cmp.config.compare.recently_used,
-            require("cmp-under-comparator").under,
-            cmp.config.compare.kind,
-          },
-        },
-        sources = cmp.config.sources({
-          { name = "async_path" },
-          { name = "neorg" },
-          { name = "orgmode" },
-          { name = "luasnip" },
-          { name = "nvim_lua" },
-          { name = "nvim_lsp" },
-          { name = "git" },
-          { name = "fuzzy_buffer", keyword_length = 5 },
-        }),
-      })
-      cmp.setup.filetype("gitcommit", {
-        sources = cmp.config.sources({
-          { name = "git" },
-          { name = "fuzzy_buffer" },
-        }),
-      })
-      require("cmp_git").setup()
-    end,
+    "saghen/blink.cmp",
     dependencies = {
-      { "FelipeLema/cmp-async-path" },
+      { "rafamadriz/friendly-snippets" },
+      { "disrupted/blink-cmp-conventional-commits" },
       {
-        "L3MON4D3/LuaSnip",
-        version = "v2.*",
-        build = "make install_jsregexp",
-      },
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "hrsh7th/cmp-nvim-lua" },
-      { "hrsh7th/cmp-path" },
-      { "lukas-reineke/cmp-under-comparator" },
-      { "petertriho/cmp-git" },
-      { "saadparwaiz1/cmp_luasnip" },
-      {
-        "tzachar/cmp-fuzzy-buffer",
-        dependencies = {
-          "tzachar/fuzzy.nvim",
-          "nvim-telescope/telescope-fzf-native.nvim",
-        },
+        "Kaiser-Yang/blink-cmp-git",
+        dependencies = { "nvim-lua/plenary.nvim" },
       },
     },
-    event = "InsertEnter",
-    enabled = false,
-  },
-  {
-    "saghen/blink.cmp",
-    dependencies = "rafamadriz/friendly-snippets",
     event = "InsertEnter",
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
+      cmdline = { enabled = false },
       keymap = { preset = "enter" },
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      signature = { enabled = true },
       sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-        cmdline = {},
+        default = { "conventional_commits", "git", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          conventional_commits = {
+            enabled = function()
+              return vim.bo.filetype == "gitcommit"
+            end,
+            module = "blink-cmp-conventional-commits",
+            name = "Conventional Commits",
+            ---@module 'blink-cmp-conventional-commits'
+            ---@type blink-cmp-conventional-commits.Options
+            opts = {},
+          },
+          git = {
+            module = "blink-cmp-git",
+            name = "Git",
+            --- @module 'blink-cmp-git'
+            --- @type blink-cmp-git.Options
+            opts = {},
+          },
+        },
       },
     },
     opts_extend = { "sources.default" },
